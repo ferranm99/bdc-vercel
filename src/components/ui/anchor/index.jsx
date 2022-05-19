6fdf304585236f2d053efefb2d1d9d3761394335
Tse <13663@wps.local>
2022-05-19 14:27:05 -0400

    Changing location and fix the rule to react/forbit-prop-types

--- src/components/ui/anchor/index.jsx
+++ src/components/ui/anchor/index.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import PropTypes from "prop-types";
 
-/* eslint-disable react/forbid-foreign-prop-types */
+/* eslint-disable react/forbid-prop-types */
 const Anchor = ({
     path,
     children,
