e950c2c6ddd0144e2f718f5421cc56bacb734eb3
Tse <13663@wps.local>
2022-05-27 17:42:57 -0400

    Fixing stupid style error.

--- src/components/widgets/logo-widget-bdc/index.jsx
+++ src/components/widgets/logo-widget-bdc/index.jsx
@@ -2,7 +2,8 @@ import PropTypes from "prop-types";
 import Image from "next/image";
 // import Logo from "@components/logo";
 
-const LogoWidget = ({ data }) => (
+// const LogoWidget = ({ data }) => (
+const LogoWidget = () => (
     <div className="text-center">
         <Image
             src="/images/bdc/logo/bdc-yellow-circle-180px.png"
@@ -11,7 +12,10 @@ const LogoWidget = ({ data }) => (
             height={180}
         />
         {/* {data?.text && <h2 className="footer-title">{data.text}</h2>} */}
-        <h2><span className="customTitleFont">Bad Dogs Company</span> embrace your <span className="customTitleFont">FIGHT</span></h2>
+        <h2>
+            <span className="customTitleFont">Bad Dogs Company</span> embrace
+            your <span className="customTitleFont">FIGHT</span>
+        </h2>
     </div>
 );
 
--- src/pages/roadmap.jsx
+++ src/pages/roadmap.jsx
@@ -4,7 +4,10 @@ import Header from "@layout/header/header-bdc";
 import Footer from "@layout/footer/footer-bdc";
 import Particles from "@ui/particles";
 import Breadcrumb from "@components/breadcrumb";
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 // import 'react-vertical-timeline-component/style.min.css';
 
 // import ContactTopArea from "@containers/contact-top";
@@ -22,105 +25,136 @@ const Roadmap = () => (
         <Header />
         <main id="main-content">
             <Particles />
-            <Breadcrumb
-                pageTitle="Bad Dogs Roadmap"
-                currentPage="Roadmap"
-            />
+            <Breadcrumb pageTitle="Bad Dogs Roadmap" currentPage="Roadmap" />
             <VerticalTimeline>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q1 2022"
-                    iconStyle={{ background: '#fff', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#fff",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Bad Dogs Genesis Collection successfully launched on OpenSea</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-success">
-                        Completed
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Bad Dogs Genesis Collection successfully launched on
+                        OpenSea
+                    </h3>
+                    <p className="text-success">Completed</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q1 2022"
-                    iconStyle={{ background: '#fff', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#fff",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Special BDC#111 Auction for Ukraine</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-success">
-                        Completed
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Special BDC#111 Auction for Ukraine
+                    </h3>
+                    <p className="text-success">Completed</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q2 2022"
-                    iconStyle={{ background: '#ffdf0b', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#fff",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Established BDC Diamond Club for passionate BDC holders.</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-success">
-                        Completed
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Established BDC Diamond Club for passionate BDC holders.
+                    </h3>
+                    <p className="text-success">Completed</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q2 2022"
-                    iconStyle={{ background: '#ffdf0b', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#ffdf0b",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Produced v1 of BDC Merchandise - hats</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-success">
-                        Completed
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Produced v1 of BDC Merchandise - hats
+                    </h3>
+                    <p className="text-success">Completed</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q2 2022"
-                    iconStyle={{ background: '#ffdf0b', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#ffdf0b",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Deployed ERC721 contract to migrate away from OpenSea ERC1155 contract.</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-success">
-                        Completed
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Deployed ERC721 contract to migrate away from OpenSea
+                        ERC1155 contract.
+                    </h3>
+                    <p className="text-success">Completed</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q2 2022"
-                    iconStyle={{ background: '#ffdf0b', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#ffdf0b",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Launched v1 of BDC Website with Migration function.</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-success">
-                        Completed
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Launched v1 of BDC Website with Migration function.
+                    </h3>
+                    <p className="text-success">Completed</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
-                    contentStyle={{ background: '#191925', color: '#fff' }}
-                    contentArrowStyle={{ borderRight: '7px solid  #191925' }}
+                    contentStyle={{ background: "#191925", color: "#fff" }}
+                    contentArrowStyle={{ borderRight: "7px solid  #191925" }}
                     date="Q2 2022"
-                    iconStyle={{ background: '#ffdf0b', textAlign: 'center', color: '#fff', paddingTop: '5px' }}
+                    iconStyle={{
+                        background: "#ffdf0b",
+                        textAlign: "center",
+                        color: "#fff",
+                        paddingTop: "5px",
+                    }}
                 >
-                    <h3 className="vertical-timeline-element-title">Establish partnership with ARC marketplace.</h3>
-                    {/* <h4 className="vertical-timeline-element-subtitle">Launched on OpenSea</h4> */}
-                    <p className="text-warning">
-                        Pending
-                    </p>
+                    <h3 className="vertical-timeline-element-title">
+                        Establish partnership with ARC marketplace.
+                    </h3>
+                    <p className="text-warning">Pending</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
-                    iconStyle={{ background: '#fff', textAlign: 'center', color: '#fff', marginTop: '50px' }}
-                // icon={<i className="feather-home icon-md" />}
+                    iconStyle={{
+                        background: "#fff",
+                        textAlign: "center",
+                        color: "#fff",
+                        marginTop: "50px",
+                    }} // icon={<i className="feather-home icon-md" />}
                 />
             </VerticalTimeline>
         </main>
