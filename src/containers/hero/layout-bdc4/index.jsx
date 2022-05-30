a4b579f957880c0ebc76238b6b0dfcb162fe151c
Tse <13663@wps.local>
2022-05-30 15:31:49 -0400

    Updated all homepage layout and will need to work with content soon.  Also need to figure out transition between sections.

---
+++ src/containers/hero/layout-bdc4/index.jsx
@@ -0,0 +1,132 @@
+import PropTypes from "prop-types";
+import Button from "@ui/button";
+import Image from "next/image";
+import Slider, { SliderItem } from "@ui/slider";
+import Portfolio from "@components/portfolio/layout-02";
+import { ButtonType, IDType, ImageType } from "@utils/types";
+
+const SliderOptions = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    cssEase: "linear",
+    adaptiveHeight: true,
+};
+
+const HeroAreaBDC4 = ({ data }) => (
+    <div className="banner-style-4 rn-section-gapTop rn-section-gapBottom">
+        <div className="container">
+            <div className="row align-items-center">
+                <h1
+                    className="title text-center"
+                    data-sal="slide-up"
+                    data-sal-delay="200"
+                    data-sal-duration="800"
+                // dangerouslySetInnerHTML={{ __html: "Bad Dogs go to Heaven" }}
+                >
+                    <span className="customTitleFont">Bad Dogs</span> go to HEAVEN
+                </h1>
+                <div className="col-xl-6 col-lg-6 col-md-12 order-2 order-lg-1 mt_md--40 mt_sm--40 pl_md--25 pl_sm--25">
+                    <div className="sldier-content">
+                        {/*data?.badge && (
+                            <span
+                                className="title-badge"
+                                data-sal="slide-up"
+                                data-sal-delay="150"
+                                data-sal-duration="800"
+                            >
+                                {data.badge}
+                            </span>
+                        )*/}
+
+                        <h3>ABOUT</h3>
+                        <p>
+                            Welcome to the Dog Pound, the underground web3
+                            social club where your NFT doubles as your
+                            membership token to the Bad Dogs Company.
+                        </p>
+                        <h3>INFO</h3>
+                        <ul className="dogs-info">
+                            <li className="dogs-info-link">
+                                Bad Dogs <a href="#arc"><mark className="arrow">ARC →</mark></a>
+                            </li>
+                            <li className="dogs-info-link">
+                                Bad Dogs <a href="#opensea"><mark className="arrow">OpenSea →</mark></a>
+                            </li>
+                            <li className="dogs-info-link">
+                                Bad Dogs <a href="#looksrare"><mark className="arrow">LooksRare →</mark></a>
+                            </li>
+                            <li className="dogs-info-link">
+                                Bad Dogs <a href="#rarity"><mark className="arrow">Rarity →</mark></a>
+                            </li>
+                        </ul>
+                        <h3>OVERVIEW</h3>
+                        <ul
+                            className="m-0 p-0"
+                            style={{
+                                overflow: "hidden",
+                                display: "flex",
+                                listStyle: "none",
+                                alignContent: "stretch",
+                            }}
+                        >
+                            <li className="float-l flex-item icon-md">
+                                <i className="feather-check-circle icon-md icon-green" />
+                                OG Membership
+                            </li>
+                            <li className="float-l ml-5 flex-item px-2 icon-md">
+                                <i className="feather-check-circle icon-md icon-green" />
+                                Premium Utility
+                            </li>
+                            <li className="float-l icon-md">
+                                <i className="feather-check-circle icon-md icon-green" />
+                                3,000 Supply
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-xl-6 col-lg-6 col-md-12 order-1 order-lg-2">
+                    <Image
+                        src="/images/bdc/nft/bdc169-667px.png"
+                        className="card-img-center"
+                        width={667}
+                        height={667}
+                        alt="..."
+                    />
+                </div>
+            </div>
+            <div className="row text-center pt-5">
+                <button type="button" className="offset-4 col-2 btn-a">
+                    BUY NOW
+                </button>
+                <hr className="horizontal" />
+                <button type="button" className="col-2 btn-b">
+                    CLAIM
+                </button>
+                <div className="col-4" />
+            </div>
+        </div>
+    </div>
+);
+
+HeroAreaBDC4.propTypes = {
+    data: PropTypes.shape({
+        badge: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        buttons: PropTypes.arrayOf(ButtonType),
+        banners: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: IDType,
+                title: PropTypes.string.isRequired,
+                client: PropTypes.string.isRequired,
+                path: PropTypes.string.isRequired,
+                image: ImageType,
+            })
+        ),
+    }),
+};
+
+export default HeroAreaBDC4;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,23 +6,24 @@ import Particles from "@ui/particles";
 import HeroArea from "@containers/hero/layout-bdc1";
 import HeroAreaBDC2 from "@containers/hero/layout-bdc2";
 import HeroAreaBDC3 from "@containers/hero/layout-bdc3";
+import HeroAreaBDC4 from "@containers/hero/layout-bdc4";
 import Image from "next/image";
 import { Tweet } from "react-twitter-widgets";
 // import Anchor from "@ui/anchor";
 
 import { normalizedData } from "@utils/methods";
 // import CTAArea from "@containers/cta";
-import Button from "react-bootstrap/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+// import Button from "react-bootstrap/Button";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 // Demo data
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
 import homepageData from "../data/homepages/home-09.json";
 import homepageDataBdc from "../data/homepages/home-bdc.json";
-import productData from "../data/products-02.json";
-import aboutData from "../data/innerpages/about.json";
+// import productData from "../data/products-02.json";
+// import aboutData from "../data/innerpages/about.json";
 // import placeholderImg from "../../public/images/client/testimonial-2.jpg";
 
 // Import Swiper React components
@@ -108,101 +109,8 @@ const Home09 = () => {
                 </div> */}
                 <div className="parallax col-12" />
 
-                <div className="profile-container customFont dog-container p-5">
-                    <div className="row" style={{ flexWrap: "wrap-reverse" }}>
-                        <div className="col-lg-6 col-sm-12 flex-end">
-                            <h3>ABOUT</h3>
-                            <p>
-                                Welcome to the Gutter, the underground web3
-                                social club where your NFT doubles as your
-                                membership token to the Gutter Cat Gang.
-                            </p>
-                            <h3>INFO</h3>
-                            <ul className="dogs-info">
-                                <li className="dogs-info-link">
-                                    Bad Dogs <a href="#opensea">Opensea</a>
-                                </li>
-                                <li className="dogs-info-link">
-                                    Bad Dogs <a href="#looksrare">LooksRare</a>
-                                </li>
-                                <li className="dogs-info-link">
-                                    Bad Dogs <a href="#rarity">Rarity</a>
-                                </li>
-                                <li className="dogs-info-link">
-                                    Bad Dogs <a href="#analytics">Analytics</a>
-                                </li>
-                            </ul>
-                            <h3>OVERVIEW</h3>
-                            <ul
-                                className="m-0 p-0"
-                                style={{
-                                    overflow: "hidden",
-                                    display: "flex",
-                                    listStyle: "none",
-                                    alignContent: "stretch",
-                                }}
-                            >
-                                <li className="float-l flex-item">
-                                    <span>
-                                        <FontAwesomeIcon
-                                            icon={faCircleCheck}
-                                            color="green"
-                                        />
-                                    </span>{" "}
-                                    OG Membership
-                                </li>
-                                <li className="float-l ml-5 flex-item px-2">
-                                    <span>
-                                        <FontAwesomeIcon
-                                            icon={faCircleCheck}
-                                            color="green"
-                                        />
-                                    </span>{" "}
-                                    Premium Utility
-                                </li>
-                                <li className="float-l">
-                                    <span>
-                                        <FontAwesomeIcon
-                                            icon={faCircleCheck}
-                                            color="green"
-                                        />
-                                    </span>{" "}
-                                    3,000 Supply
-                                </li>
-                            </ul>
-                        </div>
+                <HeroAreaBDC4 data={content4["hero-section"]} />
 
-                        {/* <Button className="btn btn-primary-alta">
-                            JOIN DISCORD
-                        </Button> */}
-
-                        <div className="col-lg-6 col-sm-12 dog-container text-center">
-                            <Image
-                                src="/images/client/testimonial-2.jpg"
-                                className="card-img-center"
-                                width={400}
-                                height={400}
-                                alt="..."
-                            />
-                        </div>
-                        <h3
-                            className="col-12 text-center customTitleFont py-5"
-                            style={{ fontSize: "5.5rem" }}
-                        >
-                            BAD DOGS
-                        </h3>
-                    </div>
-                    <div className="row">
-                        <button type="button" className="offset-4 col-2 btn-a">
-                            BUY NOW
-                        </button>
-                        <hr className="horizontal" />
-                        <button type="button" className="col-2 btn-b">
-                            CLAIM
-                        </button>
-                        <div className="col-4" />
-                    </div>
-                </div>
                 <div className="row py-5">
                     <div className="parallax-2 col-12" />
                 </div>
