bff65aba4133ab8cc4865468a45a11e3c74b342a
Louie Tse <28274608+tselouie@users.noreply.github.com>
2022-07-07 21:03:00 -0400

    disabled blockie to enable connecting wallet

--- src/layouts/header/header-bdc/index.jsx
+++ src/layouts/header/header-bdc/index.jsx
@@ -10,6 +10,7 @@ import MobileMenu from "@components/menu/mobile-menu";
 // import SearchForm from "@components/search-form/layout-01";
 // import FlyoutSearchForm from "@components/search-form/layout-02";
 import SocialWidget from "@widgets/header-social-widget-bdc";
+import HeaderAccount from "@components/header-account-bdc";
 import UserDropdown from "@components/user-dropdown";
 // import ColorSwitcher from "@components/color-switcher";
 import BurgerButton from "@ui/burger-button";
@@ -66,12 +67,10 @@ const Header = ({ className }) => {
         authenticate,
         isAuthenticated,
         isAuthenticating,
-        isInitialized,
         logout,
         chainId,
         account,
         enableWeb3,
-        isWeb3Enabled,
     } = useMoralis();
     const [isModalVisible, setIsModalVisible] = useState(false);
 
@@ -147,149 +146,9 @@ const Header = ({ className }) => {
                             <div className="setting-option header-btn">
                                 <div className="icon-box">
                                     {/* <ConnectWallet /> */}
-                                    {!isAuthenticated && (
-                                        <div className="setting-option header-btn customFont ">
-                                            <div className="icon-box">
-                                                <Button
-                                                    type="button"
-                                                    color="primary"
-                                                    size="small"
-                                                    onClick={() =>
-                                                        authenticate()
-                                                    }
-                                                >
-                                                    Wallet Connect
-                                                </Button>
-                                            </div>
-                                        </div>
-                                    )}
-                                    {isAuthenticated && (
-                                        <>
-                                            <div
-                                                style={styles.account}
-                                                onClick={() =>
-                                                    setIsModalVisible(true)
-                                                }
-                                            >
-                                                <div
-                                                    style={{
-                                                        marginRight: "5px",
-                                                        ...styles.text,
-                                                    }}
-                                                >
-                                                    <Blockie
-                                                        currentWallet
-                                                        scale={2}
-                                                    />{" "}
-                                                    {chainId}
-                                                    {getEllipsisTxt(
-                                                        account,
-                                                        6
-                                                    )}{" "}
-                                                    <button
-                                                        className="btn"
-                                                        style={{
-                                                            ...styles.logout,
-                                                        }}
-                                                        onClick={() => logout()}
-                                                        disabled={
-                                                            isAuthenticating
-                                                        }
-                                                        width={25}
-                                                        height={25}
-                                                    >
-                                                        <FontAwesomeIcon
-                                                            icon={
-                                                                faRightFromBracket
-                                                            }
-                                                            size="small"
-                                                            width={25}
-                                                            height={25}
-                                                        />
-                                                    </button>
-                                                </div>
-                                            </div>
-                                            <Modal
-                                                visible={isModalVisible}
-                                                footer={null}
-                                                onCancel={() =>
-                                                    setIsModalVisible(false)
-                                                }
-                                                bodyStyle={{
-                                                    padding: "15px",
-                                                    fontSize: "17px",
-                                                    fontWeight: "500",
-                                                }}
-                                                style={{
-                                                    fontSize: "16px",
-                                                    fontWeight: "500",
-                                                }}
-                                                width="400px"
-                                            >
-                                                Account
-                                                <Card
-                                                    style={{
-                                                        marginTop: "10px",
-                                                        borderRadius: "1rem",
-                                                    }}
-                                                    bodyStyle={{
-                                                        padding: "15px",
-                                                    }}
-                                                >
-                                                    <p>Some Address</p>
-                                                    {/* <Address
-                                                        avatar="left"
-                                                        size={6}
-                                                        copyable
-                                                        style={{
-                                                            fontSize: "20px",
-                                                        }}
-                                                    /> */}
-                                                    <div
-                                                        style={{
-                                                            marginTop: "10px",
-                                                            padding: "0 10px",
-                                                        }}
-                                                    >
-                                                        <a
-                                                            href={`${getExplorer(
-                                                                chainId
-                                                            )}/address/${account}`}
-                                                            target="_blank"
-                                                            rel="noreferrer"
-                                                        >
-                                                            <SelectOutlined
-                                                                style={{
-                                                                    marginRight:
-                                                                        "5px",
-                                                                }}
-                                                            />
-                                                            View on Explorer
-                                                        </a>
-                                                    </div>
-                                                </Card>
-                                                <Button
-                                                    size="large"
-                                                    type="button"
-                                                    style={{
-                                                        width: "100%",
-                                                        marginTop: "10px",
-                                                        borderRadius: "0.5rem",
-                                                        fontSize: "16px",
-                                                        fontWeight: "500",
-                                                    }}
-                                                    onClick={() => {
-                                                        logout();
-                                                        setIsModalVisible(
-                                                            false
-                                                        );
-                                                    }}
-                                                >
-                                                    Disconnect Wallet
-                                                </Button>
-                                            </Modal>
-                                        </>
-                                    )}
+
+                                    {/*<HeaderAccount/>*/}
+                                    <HeaderAccount />
                                 </div>
                             </div>
 
