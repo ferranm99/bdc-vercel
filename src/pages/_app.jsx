3a1efed2b53086d8a2a2f9d7cb75dc6f8201986d
Louie Tse <28274608+tselouie@users.noreply.github.com>
2022-07-30 18:34:49 -0400

    use web3modal provider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState, createContext } from "react";
 // import { ThirdwebProvider } from "@3rdweb/react";
 // import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import PropTypes from "prop-types";
@@ -18,6 +18,10 @@ import "../assets/css/carousel.css";
 import "../assets/css/timeline.min.css";
 // import "../assets/css/thirdweb.chakra.css";
 import "react-toastify/dist/ReactToastify.css";
+import { getContractValues } from "@utils/smartContractFxns";
+import { useWeb3Context } from "src/context";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
 
 // const moralisAppId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
 // const moralisServerURL = process.env.NEXT_PUBLIC_MORALIS_SERVER;
@@ -37,7 +41,11 @@ import "react-toastify/dist/ReactToastify.css";
 ReactGA.initialize("G-9P2JCG9QY5");
 
 const MyApp = ({ Component, pageProps }) => {
+    const { web3Provider, connect, disconnect, address, balance, account } =
+        useWeb3Context();
     const router = useRouter();
+    const [isWhiteListed, setWhitelistStatus] = useState(false);
+    const [contractValues, setContractValues] = useState({});
 
     useEffect(() => {
         sal({ threshold: 0.1, once: true });
@@ -50,6 +58,27 @@ const MyApp = ({ Component, pageProps }) => {
         // console.log(process.env.NEXT_PUBLIC_MORALIS_APP_ID);
         document.body.className = `${pageProps.className}`;
     });
+    useEffect(async () => {
+        const providerOptions = {
+            /* See Provider Options Section */
+        };
+
+        const web3Modal = new Web3Modal({
+            network: "mainnet", // optional
+            cacheProvider: true, // optional
+            providerOptions, // required
+        });
+
+        const instance = await web3Modal.connect();
+
+        const provider = new ethers.providers.Web3Provider(instance);
+
+        try {
+            setContractValues(await getContractValues(provider));
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
     return (
         // <ThirdwebWeb3Provider
         //     connectors={connectors}
@@ -57,13 +86,17 @@ const MyApp = ({ Component, pageProps }) => {
         // >
         <SessionProvider session={pageProps.session} refetchInterval={0}>
             <SSRProvider>
-                <Web3ContextProvider>
-                    {/* <MoralisProvider appId={moralisAppId} serverUrl={moralisServerURL}> */}
-                    <ThemeProvider defaultTheme="dark">
-                        <Component {...pageProps} />
-                    </ThemeProvider>
-                    {/* </MoralisProvider> */}
-                </Web3ContextProvider>
+                <ContractContext.Provider
+                    value={{ isWhiteListed, contractValues }}
+                >
+                    <Web3ContextProvider>
+                        {/* <MoralisProvider appId={moralisAppId} serverUrl={moralisServerURL}> */}
+                        <ThemeProvider defaultTheme="dark">
+                            <Component {...pageProps} />
+                        </ThemeProvider>
+                        {/* </MoralisProvider> */}
+                    </Web3ContextProvider>
+                </ContractContext.Provider>
             </SSRProvider>
         </SessionProvider>
     );
@@ -77,5 +110,5 @@ MyApp.propTypes = {
         className: PropTypes.string,
     }),
 };
-
+export const ContractContext = createContext();
 export default MyApp;
