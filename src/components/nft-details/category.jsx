310a142402e12b4cb71cab8d56e7f746ff4bc912
Samson Tse <tse.samson@gmail.com>
2022-08-28 19:26:38 -0400

    Updated category to show walet address but need to create new component for that.

--- src/components/nft-details/category.jsx
+++ src/components/nft-details/category.jsx
@@ -2,14 +2,16 @@ import PropTypes from "prop-types";
 import clsx from "clsx";
 import TopSeller from "@components/top-seller/layout-01";
 import { ImageType } from "@utils/types";
+import { truncateEthAddress } from "@utils/formatters";
 
 const NftCategory = ({ className, owner }) => (
     <div className={clsx("catagory", className)}>
         <span>
-            Catagory <span className="color-body">10% royalties</span>
+            Owner
+            {/* Catagory <span className="color-body">10% royalties</span> */}
         </span>
         <TopSeller
-            name={owner.name}
+            name={truncateEthAddress(owner.name)}
             slug={owner.slug}
             image={{ src: owner.image.src, width: 44, height: 44 }}
         />
